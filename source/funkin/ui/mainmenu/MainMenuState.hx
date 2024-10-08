9508c27627ec568e468c9623d7ee0e95af0714bc
Forthfireboy <forthfireboy@gmail.com>
2024-10-08 13:04:28 +0700

    Update MainMenuState.hx

--- source/funkin/ui/mainmenu/MainMenuState.hx
+++ source/funkin/ui/mainmenu/MainMenuState.hx
@@ -11,7 +11,7 @@ import flixel.effects.FlxFlicker;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.util.typeLimit.NextState;
 import flixel.util.FlxColor;
-import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxTypedGroup;
 import flixel.text.FlxText;
 import funkin.data.song.SongData.SongMusicData;
 import flixel.tweens.FlxEase;
@@ -20,14 +20,12 @@ import funkin.audio.FunkinSound;
 import flixel.tweens.FlxTween;
 import funkin.ui.MusicBeatState;
 import flixel.util.FlxTimer;
-import funkin.ui.AtlasMenuList;
 import funkin.ui.freeplay.FreeplayState;
 import funkin.ui.MenuList;
 import funkin.ui.title.TitleState;
 import funkin.ui.story.StoryMenuState;
 import funkin.ui.Prompt;
 import funkin.util.WindowUtil;
-import funkin.mobile.util.TouchUtil;
 #if FEATURE_DISCORD_RPC
 import funkin.api.discord.DiscordClient;
 #end
@@ -36,435 +34,129 @@ import funkin.ui.NgPrompt;
 import io.newgrounds.NG;
 #end
 
-class MainMenuState extends MusicBeatState
-{
-  var menuItems:MenuTypedList<AtlasMenuItem>;
+class MainMenuState extends MusicBeatState {
+    public static var curSelected:Int = 0;
 
-  var magenta:FlxSprite;
-  var camFollow:FlxObject;
+    var menuItems:FlxTypedGroup<FlxSprite>;
+    var magenta:FlxSprite;
+    var camFollow:FlxObject;
 
-  var overrideMusic:Bool = false;
+    var optionShit:Array<String> = [
+        'storymode',
+        'freeplay',
+        'options',
+        'credits'
+    ];
 
-  static var rememberedSelectedIndex:Int = 0;
-
-  public function new(?_overrideMusic:Bool = false)
-  {
-    super();
-    overrideMusic = _overrideMusic;
-  }
-
-  override function create():Void
-  {
-    #if FEATURE_DISCORD_RPC
-    DiscordClient.instance.setPresence({state: "In the Menus", details: null});
-    #end
-
-    FlxG.cameras.reset(new FunkinCamera('mainMenu'));
-
-    transIn = FlxTransitionableState.defaultTransIn;
-    transOut = FlxTransitionableState.defaultTransOut;
-
-    if (!overrideMusic) playMenuMusic();
-
-    // We want the state to always be able to begin with being able to accept inputs and show the anims of the menu items.
-    persistentUpdate = true;
-    persistentDraw = true;
-
-    var bg:FlxSprite = new FlxSprite(Paths.image('menuBG'));
-    bg.scrollFactor.x = 0;
-    bg.scrollFactor.y = 0.17;
-    bg.setGraphicSize(Std.int(bg.width * 1.2));
-    bg.updateHitbox();
-    bg.screenCenter();
-    add(bg);
-
-    camFollow = new FlxObject(0, 0, 1, 1);
-    add(camFollow);
-
-    magenta = new FlxSprite(Paths.image('menuBGMagenta'));
-    magenta.scrollFactor.x = bg.scrollFactor.x;
-    magenta.scrollFactor.y = bg.scrollFactor.y;
-    magenta.setGraphicSize(Std.int(bg.width));
-    magenta.updateHitbox();
-    magenta.x = bg.x;
-    magenta.y = bg.y;
-    magenta.visible = false;
-
-    // TODO: Why doesn't this line compile I'm going fucking feral
-
-    if (Preferences.flashingLights) add(magenta);
-
-    menuItems = new MenuTypedList<AtlasMenuItem>();
-    add(menuItems);
-    menuItems.onChange.add(onMenuItemChange);
-    menuItems.onAcceptPress.add(function(_) {
-      FlxFlicker.flicker(magenta, 1.1, 0.15, false, true);
-    });
-
-    menuItems.enabled = true; // can move on intro
-    createMenuItem('storymode', 'mainmenu/storymode', function() startExitState(() -> new StoryMenuState()));
-    createMenuItem('freeplay', 'mainmenu/freeplay', function() {
-      persistentDraw = true;
-      persistentUpdate = false;
-      // Freeplay has its own custom transition
-      FlxTransitionableState.skipNextTransIn = true;
-      FlxTransitionableState.skipNextTransOut = true;
-
-      #if FEATURE_DEBUG_FUNCTIONS
-      // Debug function: Hold SHIFT when selecting Freeplay to swap character without the char select menu
-      var targetCharacter:Null<String> = (FlxG.keys.pressed.SHIFT) ? (FreeplayState.rememberedCharacterId == "pico" ? "bf" : "pico") : null;
-      #else
-      var targetCharacter:Null<String> = null;
-      #end
-
-      openSubState(new FreeplayState(
-        {
-          character: targetCharacter
-        }));
-    });
-
-    #if FEATURE_OPEN_URL
-    // In order to prevent popup blockers from triggering,
-    // we need to open the link as an immediate result of a keypress event,
-    // so we can't wait for the flicker animation to complete.
-    var hasPopupBlocker = #if web true #else false #end;
-    createMenuItem('merch', 'mainmenu/merch', selectMerch, hasPopupBlocker);
-    #end
-
-    createMenuItem('options', 'mainmenu/options', function() {
-      startExitState(() -> new funkin.ui.options.OptionsState());
-    });
-
-    createMenuItem('credits', 'mainmenu/credits', function() {
-      startExitState(() -> new funkin.ui.credits.CreditsState());
-    });
-
-    // Reset position of menu items.
-    var spacing = 160;
-    var top = (FlxG.height - (spacing * (menuItems.length - 1))) / 2;
-    for (i in 0...menuItems.length)
-    {
-      var menuItem = menuItems.members[i];
-      menuItem.x = FlxG.width / 2;
-      menuItem.y = top + spacing * i;
-      menuItem.scrollFactor.x = 0.0;
-      // This one affects how much the menu items move when you scroll between them.
-      menuItem.scrollFactor.y = 0.4;
+    public function new(?_overrideMusic:Bool = false) {
+        super();
     }
 
-    menuItems.selectItem(rememberedSelectedIndex);
+    override function create():Void {
+        #if FEATURE_DISCORD_RPC
+        DiscordClient.instance.setPresence({state: "In the Menus", details: null});
+        #end
 
-    resetCamStuff();
+        FlxG.cameras.reset(new FunkinCamera('mainMenu'));
 
-    // reset camera when debug menu is closed
-    subStateClosed.add(_ -> resetCamStuff(false));
+        transIn = FlxTransitionableState.defaultTransIn;
+        transOut = FlxTransitionableState.defaultTransOut;
 
-    subStateOpened.add(sub -> {
-      if (Type.getClass(sub) == FreeplayState)
-      {
-        new FlxTimer().start(0.5, _ -> {
-          magenta.visible = false;
-        });
-      }
-    });
+        playMenuMusic();
 
-    // FlxG.camera.setScrollBounds(bg.x, bg.x + bg.width, bg.y, bg.y + bg.height * 1.2);
+        var bg:FlxSprite = new FlxSprite(Paths.image('menuBG'));
+        bg.scrollFactor.x = 0;
+        bg.scrollFactor.y = 0.17;
+        bg.setGraphicSize(Std.int(bg.width * 1.2));
+        bg.updateHitbox();
+        bg.screenCenter();
+        add(bg);
 
-    #if mobile
-    addBackButton(FlxG.width * 0.03, FlxG.height * 0.79, FlxColor.BLACK, goBack);
-    #end
+        camFollow = new FlxObject(0, 0, 1, 1);
+        add(camFollow);
 
-    super.create();
+        magenta = new FlxSprite(Paths.image('menuBGMagenta'));
+        magenta.scrollFactor.x = bg.scrollFactor.x;
+        magenta.scrollFactor.y = bg.scrollFactor.y;
+        magenta.setGraphicSize(Std.int(bg.width));
+        magenta.updateHitbox();
+        magenta.x = bg.x;
+        magenta.y = bg.y;
+        magenta.visible = false;
 
-    // This has to come AFTER!
-    this.leftWatermarkText.text = Constants.VERSION;
-    // this.rightWatermarkText.text = "blablabla test";
+        if (Preferences.flashingLights) add(magenta);
 
-    // NG.core.calls.event.logEvent('swag').send();
+        menuItems = new FlxTypedGroup<FlxSprite>();
+        add(menuItems);
 
-    // Leave this here do not change it -Zack
-    #if mobile
-    camFollow.setPosition(640, 280);
-    FlxG.camera.snapToTarget();
-    #end
-  }
+        // Create menu items
+        for (i in 0...optionShit.length) {
+            var offset:Float = 160 - (Math.max(optionShit.length, 4) - 4) * 80;
+            var menuItem:FlxSprite = new FlxSprite(0, (i * 140) + offset);
+            menuItem.frames = Paths.getSparrowAtlas('mainmenu/' + optionShit[i]);
+            menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
+            menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
+            menuItem.animation.play('idle');
+            menuItems.add(menuItem);
+            menuItem.scrollFactor.set(0, 0.4);
+            menuItem.updateHitbox();
+            menuItem.screenCenter(X);
+        }
 
-  function playMenuMusic():Void
-  {
-    FunkinSound.playMusic('freakyMenu',
-      {
-        overrideExisting: true,
-        restartTrack: false
-      });
-  }
+        var psychVer:FlxText = new FlxText(12, FlxG.height - 44, 0, "Your Game Version", 12);
+        psychVer.scrollFactor.set();
+        psychVer.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(psychVer);
 
-  function resetCamStuff(?snap:Bool = true):Void
-  {
-    FlxG.camera.follow(camFollow, null, 0.06);
+        changeItem(); // Set initial selected item
 
-    if (snap) FlxG.camera.snapToTarget();
-  }
-
-  function createMenuItem(name:String, atlas:String, callback:Void->Void, fireInstantly:Bool = false):Void
-  {
-    var item = new AtlasMenuItem(name, Paths.getSparrowAtlas(atlas), callback);
-    item.fireInstantly = fireInstantly;
-    item.ID = menuItems.length;
-
-    item.scrollFactor.set();
-
-    // Set the offset of the item so the sprite is centered on the origin.
-    item.centered = true;
-    item.changeAnim('idle');
-
-    menuItems.addItem(name, item);
-  }
-
-  override function closeSubState():Void
-  {
-    magenta.visible = false;
-    super.closeSubState();
-  }
-
-  override function finishTransIn():Void
-  {
-    super.finishTransIn();
-
-    // menuItems.enabled = true;
-
-    // #if newgrounds
-    // if (NGio.savedSessionFailed)
-    // 	showSavedSessionFailed();
-    // #end
-  }
-
-  function onMenuItemChange(selected:MenuListItem)
-  {
-    camFollow.setPosition(selected.getGraphicMidpoint().x, selected.getGraphicMidpoint().y);
-  }
-
-  #if FEATURE_OPEN_URL
-  function selectDonate()
-  {
-    WindowUtil.openURL(Constants.URL_ITCH);
-  }
-
-  function selectMerch()
-  {
-    WindowUtil.openURL(Constants.URL_MERCH);
-  }
-  #end
-
-  #if newgrounds
-  function selectLogin()
-  {
-    openNgPrompt(NgPrompt.showLogin());
-  }
-
-  function selectLogout()
-  {
-    openNgPrompt(NgPrompt.showLogout());
-  }
-
-  function showSavedSessionFailed()
-  {
-    openNgPrompt(NgPrompt.showSavedSessionFailed());
-  }
-
-  /**
-   * Calls openPrompt and redraws the login/logout button
-   * @param prompt
-   * @param onClose
-   */
-  public function openNgPrompt(prompt:Prompt, ?onClose:Void->Void)
-  {
-    var onPromptClose = checkLoginStatus;
-    if (onClose != null)
-    {
-      onPromptClose = function() {
-        checkLoginStatus();
-        onClose();
-      }
+        super.create();
     }
 
-    openPrompt(prompt, onPromptClose);
-  }
+    override function update(elapsed:Float):Void {
+        if (FlxG.sound.music.volume < 0.8) {
+            FlxG.sound.music.volume += 0.5 * elapsed;
+        }
 
-  function checkLoginStatus()
-  {
-    var prevLoggedIn = menuItems.has("logout");
-    if (prevLoggedIn && !NGio.isLoggedIn) menuItems.resetItem("login", "logout", selectLogout);
-    else if (!prevLoggedIn && NGio.isLoggedIn) menuItems.resetItem("logout", "login", selectLogin);
-  }
-  #end
+        if (controls.UI_UP_P) changeItem(-1);
+        if (controls.UI_DOWN_P) changeItem(1);
+        if (controls.ACCEPT_P) selectItem();
 
-  public function openPrompt(prompt:Prompt, onClose:Void->Void):Void
-  {
-    menuItems.enabled = false;
-    persistentUpdate = false;
-
-    prompt.closeCallback = function() {
-      menuItems.enabled = true;
-      if (onClose != null) onClose();
+        super.update(elapsed);
     }
 
-    openSubState(prompt);
-  }
-
-  function startExitState(state:NextState):Void
-  {
-    menuItems.enabled = false; // disable for exit
-    rememberedSelectedIndex = menuItems.selectedIndex;
-
-    var duration = 0.4;
-    menuItems.forEach(function(item) {
-      if (menuItems.selectedIndex != item.ID)
-      {
-        FlxTween.tween(item, {alpha: 0}, duration, {ease: FlxEase.quadOut});
-      }
-      else
-      {
-        item.visible = false;
-      }
-    });
-
-    new FlxTimer().start(duration, function(_) FlxG.switchState(state));
-  }
-
-  override function update(elapsed:Float):Void
-  {
-    super.update(elapsed);
-
-    Conductor.instance.update();
-
-    // Open the debug menu, defaults to ` / ~
-    // This includes stuff like the Chart Editor, so it should be present on all builds.
-    if (controls.DEBUG_MENU)
-    {
-      persistentUpdate = false;
-
-      FlxG.state.openSubState(new DebugMenuSubState());
-    }
-
-    #if FEATURE_DEBUG_FUNCTIONS
-    // Ctrl+Alt+Shift+P = Character Unlock screen
-    // Ctrl+Alt+Shift+W = Meet requirements for Pico Unlock
-    // Ctrl+Alt+Shift+M = Revoke requirements for Pico Unlock
-    // Ctrl+Alt+Shift+R = Score/Rank conflict test
-    // Ctrl+Alt+Shift+N = Mark all characters as not seen
-    // Ctrl+Alt+Shift+E = Dump save data
-    // Ctrl+Alt+Shift+L = Force crash and create a log dump
-
-    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.ALT && FlxG.keys.pressed.SHIFT && FlxG.keys.justPressed.P)
-    {
-      FlxG.switchState(() -> new funkin.ui.charSelect.CharacterUnlockState('pico'));
-    }
-
-    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.ALT && FlxG.keys.pressed.SHIFT && FlxG.keys.justPressed.W)
-    {
-      FunkinSound.playOnce(Paths.sound('confirmMenu'));
-      // Give the user a score of 1 point on Weekend 1 story mode (Easy difficulty).
-      // This makes the level count as cleared and displays the songs in Freeplay.
-      funkin.save.Save.instance.setLevelScore('weekend1', 'easy',
-        {
-          score: 1,
-          tallies:
-            {
-              sick: 0,
-              good: 0,
-              bad: 0,
-              shit: 0,
-              missed: 0,
-              combo: 0,
-              maxCombo: 0,
-              totalNotesHit: 0,
-              totalNotes: 0,
-            }
+    function playMenuMusic():Void {
+        FunkinSound.playMusic('freakyMenu', {
+            overrideExisting: true,
+            restartTrack: false
         });
     }
 
-    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.ALT && FlxG.keys.pressed.SHIFT && FlxG.keys.justPressed.M)
-    {
-      FunkinSound.playOnce(Paths.sound('confirmMenu'));
-      // Give the user a score of 0 points on Weekend 1 story mode (all difficulties).
-      // This makes the level count as uncleared and no longer displays the songs in Freeplay.
-      for (diff in ['easy', 'normal', 'hard'])
-      {
-        funkin.save.Save.instance.setLevelScore('weekend1', diff,
-          {
-            score: 0,
-            tallies:
-              {
-                sick: 0,
-                good: 0,
-                bad: 0,
-                shit: 0,
-                missed: 0,
-                combo: 0,
-                maxCombo: 0,
-                totalNotesHit: 0,
-                totalNotes: 0,
-              }
-          });
-      }
+    function changeItem(dir:Int = 0):Void {
+        menuItems.members[curSelected].animation.play('idle');
+        curSelected += dir;
+
+        if (curSelected < 0) curSelected = menuItems.length - 1;
+        if (curSelected >= menuItems.length) curSelected = 0;
+
+        menuItems.members[curSelected].animation.play('selected');
+        camFollow.setPosition(menuItems.members[curSelected].x, menuItems.members[curSelected].y);
     }
 
-    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.ALT && FlxG.keys.pressed.SHIFT && FlxG.keys.justPressed.R)
-    {
-      // Give the user a hypothetical overridden score,
-      // and see if we can maintain that golden P rank.
-      funkin.save.Save.instance.setSongScore('tutorial', 'easy',
-        {
-          score: 1234567,
-          tallies:
-            {
-              sick: 0,
-              good: 0,
-              bad: 0,
-              shit: 1,
-              missed: 0,
-              combo: 0,
-              maxCombo: 0,
-              totalNotesHit: 1,
-              totalNotes: 10,
-            }
-        });
+    function selectItem():Void {
+        switch (curSelected) {
+            case 0:
+                FlxG.switchState(() -> new StoryMenuState());
+            case 1:
+                openSubState(new FreeplayState());
+            case 2:
+                FlxG.switchState(() -> new funkin.ui.options.OptionsState());
+            case 3:
+                FlxG.switchState(() -> new funkin.ui.credits.CreditsState());
+        }
     }
 
-    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.ALT && FlxG.keys.pressed.SHIFT && FlxG.keys.justPressed.N)
-    {
-      @:privateAccess
-      {
-        funkin.save.Save.instance.data.unlocks.charactersSeen = ["bf"];
-        funkin.save.Save.instance.data.unlocks.oldChar = false;
-      }
+    public function goBack():Void {
+        FlxG.switchState(() -> new TitleState());
     }
-
-    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.ALT && FlxG.keys.pressed.SHIFT && FlxG.keys.justPressed.E)
-    {
-      funkin.save.Save.instance.debug_dumpSave();
-    }
-    #end
-
-    if (FlxG.sound.music != null && FlxG.sound.music.volume < 0.8)
-    {
-      FlxG.sound.music.volume += 0.5 * elapsed;
-    }
-
-    if (_exiting) menuItems.enabled = false;
-
-    if (controls.BACK)
-    {
-      goBack();
-    }
-  }
-
-  public function goBack():Void
-  {
-    if (menuItems.enabled && !menuItems.busy)
-    {
-      FlxG.switchState(() -> new TitleState());
-      FunkinSound.playOnce(Paths.sound('cancelMenu'));
-    }
-  }
 }
